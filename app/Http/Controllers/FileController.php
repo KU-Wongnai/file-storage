87bc6c977a6ff7c6ec2439c0118d1abcbc016770
Qu1etboy <nonzagreanthai@gmail.com>
2023-09-01 12:36:01 +0700

    feat: upload file

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function saveFile(Request $request) {
+        
+        $request->validate([
+            'file' => ['required', 'mimes:jpg,png,jpeg'],
+            'file_name' => ['required', 'string']
+        ]);
+
+        $file = $request->file('file');
+        $file_name = $request->get('file_name');
+        $path = $file->storeAs('public', $file_name . '.' . $file->getClientOriginalExtension());
+       
+        $file_path = str_replace('public/', '', $path);
+    
+        return response()->json([
+            'message' => 'File uploaded successfully', 
+            'success' => true,
+            'data' => asset('storage/' . $file_path),
+        ]);
+    }
 }
